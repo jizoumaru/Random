4ab3d2929b5e63d921281b00438945d726f7fad0
jizoumaru <40907331+jizoumaru@users.noreply.github.com>
2020-12-04 23:13:23 +0900

    Update XorshiftAdd.java

--- XorshiftAdd.java
+++ XorshiftAdd.java
@@ -1,42 +1,29 @@
+import java.nio.ByteBuffer;
+import java.security.SecureRandom;
+import java.util.function.IntSupplier;
 
-public class XorshiftAdd {
-	public static void main(String[] args) {
-		var xa = new XorshiftAdd(1234);
-		
-		for (var i = 0; i < 100; i++) {
-			System.out.println(Integer.toUnsignedString(xa.generate()));
+public class XorshiftAdd implements IntSupplier {
+	private final int[] s;
+
+	public XorshiftAdd() {
+		var s = new int[4];
+		var b = ByteBuffer.wrap(SecureRandom.getSeed(Integer.SIZE * s.length));
+		for (var i = 0; i < s.length; i++) {
+			s[i] = b.getInt(Integer.SIZE * i);
 		}
-	}
-	
-	private final int[] state;
-
-	public XorshiftAdd(int seed) {
-		state = new int[] { seed, 0, 0, 0 };
-
-		for (int i = 1; i < 8; i++) {
-			state[i & 3] ^= i + 1812433253
-					* (state[(i - 1) & 3]
-							^ (state[(i - 1) & 3] >>> 30));
-		}
-
-		for (int i = 0; i < 8; i++) {
-			xsadd_next_state();
-		}
+		this.s = s;
 	}
 
-	private void xsadd_next_state() {
-		var t = state[0];
+	@Override
+	public int getAsInt() {
+		var t = s[0];
 		t ^= t << 15;
 		t ^= t >>> 18;
-		t ^= state[3] << 11;
-		state[0] = state[1];
-		state[1] = state[2];
-		state[2] = state[3];
-		state[3] = t;
-	}
-
-	public int generate() {
-		xsadd_next_state();
-		return state[3] + state[2];
+		t ^= s[3] << 11;
+		s[0] = s[1];
+		s[1] = s[2];
+		s[2] = s[3];
+		s[3] = t;
+		return s[3] + s[2];
 	}
 }
