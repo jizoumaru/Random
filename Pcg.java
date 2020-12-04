f1bf5890549554d3bdc2df104dad8bc745d30e9b
jizoumaru <40907331+jizoumaru@users.noreply.github.com>
2020-12-04 21:54:24 +0900

    Update Pcg.java

--- Pcg.java
+++ Pcg.java
@@ -1,36 +1,25 @@
-final class Pcg {
-	public static void main(String[] args) {
-		Pcg pcg = new Pcg();
-		for (int i = 0; i < 100; i++) {
-			System.out.println(pcg.next());
-		}
-	}
-	
-	private long state;
-	private long inc;
-	
+final class Pcg implements IntSupplier {
+	private final long i;
+	private long s;
+
 	Pcg() {
 		SecureRandom sr = new SecureRandom();
-		init(sr.nextLong(), sr.nextLong());
+		long i = sr.nextLong();
+		long s = sr.nextLong();
+
+		this.s = 0L;
+		this.i = (i << 1L) | 1L;
+		getAsInt();
+		this.s += s;
+		getAsInt();
 	}
-	
-	Pcg(long initstate, long initseq) {
-		init(initstate, initseq);
-	}
-	
-	private void init(long initstate, long initseq) {
-	    state = 0L;
-	    inc = (initseq << 1L) | 1L;
-	    next();
-	    state += initstate;
-	    next();
-	}
-	
-	public int next() {
-	    long oldstate = state;
-	    state = oldstate * 6364136223846793005L + inc;
-	    int xorshifted = (int)(((oldstate >>> 18L) ^ oldstate) >>> 27L);
-	    int rot = (int)(oldstate >>> 59L);
-	    return (xorshifted >>> rot) | (xorshifted << ((-rot) & 31));
+
+	@Override
+	public int getAsInt() {
+		long o = s;
+		s = o * 6364136223846793005L + i;
+		int x = (int) (((o >>> 18L) ^ o) >>> 27L);
+		int r = (int) (o >>> 59L);
+		return (x >>> r) | (x << ((-r) & 31));
 	}
 }
