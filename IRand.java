735b1d1fd7cb3da2dcc15a8884c7c69a93412651
jizoumaru <40907331+jizoumaru@users.noreply.github.com>
2020-12-27 19:08:51 +0900

    Update IRand.java

--- IRand.java
+++ IRand.java
@@ -2,64 +2,60 @@ interface IRand {
 	default boolean nextBoolean() {
 		return (nextInt() & 1) == 1;
 	}
-	
+
 	default int nextInt() {
-		return (int)nextLong();
+		return (int) nextLong();
 	}
-	
-	default int nextInt(int from, int to) {
-		if (from < 0 && from + Integer.MAX_VALUE < to) {
+
+	default int nextInt(int count) {
+		var l = (Integer.MAX_VALUE - count + 1) % count;
+		var n = nextInt() & Integer.MAX_VALUE;
+		while (n < l) {
+			n = nextInt() & Integer.MAX_VALUE;
+		}
+		return n % count;
+	}
+
+	default int nextInt(int first, int last) {
+		if (first < 0 && first + Integer.MAX_VALUE < last) {
 			var n = nextInt();
-			
-			while (n < from || to < n) {
+			while (n < first || last < n) {
 				n = nextInt();
 			}
-			
 			return n;
 		} else {
-			if (to - from == Integer.MAX_VALUE) {
-				return (nextInt() & Integer.MAX_VALUE) + from;
+			if (last - first == Integer.MAX_VALUE) {
+				return (nextInt() & Integer.MAX_VALUE) + first;
 			}
-			
-			var r = to - from + 1;
-			var l = (Integer.MAX_VALUE - r + 1) % r;
-			var n = nextInt() & Integer.MAX_VALUE;
-			
-			while (n < l) {
-				n = nextInt() & Integer.MAX_VALUE;
-			}
-			
-			return n % r + from;
+			return nextInt(last - first + 1) + first;
 		}
 	}
 
 	default long nextLong() {
-		return (long)nextInt() << 32 | (long)nextInt() & 0xFFFFFFFFL;
+		return (long) nextInt() << 32 | (long) nextInt() & 0xFFFFFFFFL;
 	}
-	
-	default long nextLong(long from, long to) {
-		if (from < 0 && from + Long.MAX_VALUE < to) {
+
+	default long nextLong(long count) {
+		var l = (Long.MAX_VALUE - count + 1L) % count;
+		var n = nextLong() & Long.MAX_VALUE;
+		while (n < l) {
+			n = nextLong() & Long.MAX_VALUE;
+		}
+		return n % count;
+	}
+
+	default long nextLong(long first, long last) {
+		if (first < 0 && first + Long.MAX_VALUE < last) {
 			var n = nextLong();
-			
-			while (n < from || to < n) {
+			while (n < first || last < n) {
 				n = nextLong();
 			}
-			
 			return n;
 		} else {
-			if (to - from == Long.MAX_VALUE) {
-				return (nextLong() & Long.MAX_VALUE) + from;
+			if (last - first == Long.MAX_VALUE) {
+				return (nextLong() & Long.MAX_VALUE) + first;
 			}
-			
-			var r = to - from + 1L;
-			var l = (Long.MAX_VALUE - r + 1L) % r;
-			var n = nextLong() & Long.MAX_VALUE;
-			
-			while (n < l) {
-				n = nextLong() & Long.MAX_VALUE;
-			}
-			
-			return n % r + from;
+			return nextLong(last - first + 1L) + first;
 		}
 	}
 
