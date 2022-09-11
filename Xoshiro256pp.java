6e74c9703d78212c8c770464edfbaf51a87abadb
jizoumaru <40907331+jizoumaru@users.noreply.github.com>
2022-09-12 01:16:56 +0900

    Update Xoshiro256pp.java

--- Xoshiro256pp.java
+++ Xoshiro256pp.java
@@ -1,29 +1,36 @@
 import java.nio.ByteBuffer;
 import java.security.SecureRandom;
-import java.util.function.LongSupplier;
+import java.util.random.RandomGenerator;
 
-public final class Xoshiro256pp implements LongSupplier {
-	public static void main(String[] args) {
+public final class Xoshiro256pp implements RandomGenerator
+{
+	public static void main(String[] args)
+	{
 		var x = new Xoshiro256pp();
-		
-		for (var i = 0; i < 100; i++) {
-			System.out.println(x.getAsLong());
-		}
+		x.ints().limit(100).forEach(System.out::println);
 	}
 
 	private final long[] s;
 
-	public Xoshiro256pp() {
+	public Xoshiro256pp()
+	{
 		var s = new long[4];
-		var b = ByteBuffer.wrap(SecureRandom.getSeed(Long.SIZE * s.length));
-		for (int i = 0; i < s.length; i++) {
-			s[i] = b.getLong(Long.SIZE * i); 
+		var b = ByteBuffer.wrap(SecureRandom.getSeed(Long.BYTES * s.length));
+		for (int i = 0; i < s.length; i++)
+		{
+			s[i] = b.getLong();
 		}
 		this.s = s;
 	}
-	
+
+	private static long rotl(long x, int k)
+	{
+		return (x << k) | (x >>> (64 - k));
+	}
+
 	@Override
-	public long getAsLong() {
+	public long nextLong()
+	{
 		long r = rotl(s[0] + s[3], 23) + s[0];
 		long t = s[1] << 17;
 		s[2] ^= s[0];
@@ -33,9 +40,5 @@ public final class Xoshiro256pp implements LongSupplier {
 		s[2] ^= t;
 		s[3] = rotl(s[3], 45);
 		return r;
-	}
-	
-	private static long rotl(long x, int k) {
-		return (x << k) | (x >>> (64 - k));
 	}
 }
